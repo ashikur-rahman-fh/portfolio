cb3d4e350736079e94d9b9e738490aacc787e5f5
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-09-29 11:33:17 -0400

    Add dummy circle style

--- src/About/constants.js
+++ src/About/constants.js
@@ -1,2 +1,4 @@
 export const TITLE = <span>Hey, I'm a Software Engineer.</span>;
 export const DETAILED_INFO = "Hi, I'm Ashikur Rahman. A competitive programmer and problem solver. I try to build the best quality software possible."
+
+export const DUMMY_CIRCLE_COUNT = 40;
--- src/About/index.jsx
+++ src/About/index.jsx
@@ -2,15 +2,27 @@ import React from "react";
 
 import displayPicture from "../resource/image/dp.png";
 
-import { DETAILED_INFO, TITLE } from "./constants";
+import { DETAILED_INFO, TITLE, DUMMY_CIRCLE_COUNT } from "./constants";
 
 import "./style.css";
 
 const About = (props) => {
 
+    const renderDummyChildCircles = () => {
+        let dummyElements = [];
+        for (let elementCount = 0; elementCount < DUMMY_CIRCLE_COUNT; elementCount++) {
+            dummyElements.push(<div key={elementCount.toString()} className="dummy-circle-child"></div>);
+        }
+
+        return dummyElements;
+    };
+
     return (
         <div className="about-container outmost-container">
-            <div className="dummy-circle">nothing</div>
+            <div className="dummy-circle"></div>
+            <div className="dummy-circle-container">
+                {renderDummyChildCircles()}
+            </div>
 
             <div className="bio-container">
                 <div className="key-text" >{TITLE}</div>

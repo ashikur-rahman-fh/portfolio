b82ce389a8f48b9d017a73f5ac410a2750918d08
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-09-30 07:03:06 -0400

    Add test rain link

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import Skills from "../Skills";
 import About from "../About";
 
+import { Link } from "react-router-dom";
+
 const HomePage = (props) => {
 
     return (
         <React.Fragment>
             <About />
             <Skills />
+            <Link to="rain" >see rain</Link>
         </React.Fragment>
     );
 };
